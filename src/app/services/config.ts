460a19edce598b1bd42c0808f44403e30f61fdb6
AlexisMG2020 <127353210+AlexisMG2020@users.noreply.github.com>
2024-03-20 14:54:50 -0600

    front123454

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,12 +1,12 @@
 
 // environment.dev.ts
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:3000',
-// };
-
+/* export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+};
+ */
 // environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://859b-187-190-198-147.ngrok-free.app',
+  apiUrl: 'https://ca25-187-190-198-147.ngrok-free.app',
 };
